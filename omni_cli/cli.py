22858161a4c2e59665a871622bb103ba8705389e
btraven <btr@dev>
2023-03-13 13:00:46 +0100

    feat: dump bench stages

--- omni_cli/cli.py
+++ omni_cli/cli.py
@@ -2,7 +2,7 @@ import click
 
 from .config import init_dirs
 from .sync import download_bench_data
-from .benchmarks import benchmark_list
+from .benchmarks import benchmark_list, stage_list
 
 @click.group()
 def run():
@@ -15,30 +15,48 @@ def describe():
 
 @click.command()
 def init():
+    """Initialize omnibenchmark cache"""
     click.echo(f"Initializing omnibenchmark cache...")
     init_dirs()
     download_bench_data()
 
 @click.command()
 def update():
+    """Update omnibenchmark cache"""
     click.echo(f"Updating omnibenchmark cache...")
     download_bench_data(force=True)
 
+@click.group()
+def bench():
+    """Inspect benchmarks"""
+    pass
+
 @click.command()
-def benchmark():
+def ls():
     for bench in benchmark_list():
         click.echo(f"{bench}")
 
 @click.command()
+@click.argument('bench_id')
+def stages(bench_id):
+    for stage in stage_list(bench_id):
+        click.echo(f"{bench_id}:{stage}")
+
+@click.command()
 def dataset():
+    """Inspect datasets"""
     click.echo(f"Doing stuff with datasets...")
 
 @click.command()
 def workflow():
+    """Interacts with workflows"""
     click.echo(f"Doing stuff with workflows...")
+
+bench.add_command(ls)
+bench.add_command(stages)
 
 run.add_command(init)
 run.add_command(update)
-run.add_command(benchmark)
+run.add_command(bench)
 run.add_command(dataset)
 run.add_command(workflow)
