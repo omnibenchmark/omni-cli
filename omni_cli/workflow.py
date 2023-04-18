6c31f5f46e04c4e05073f80376a217ed6650a41d
btraven <btr@dev>
2023-04-18 17:47:35 +0200

    generate graph

--- omni_cli/workflow.py
+++ omni_cli/workflow.py
@@ -1,30 +1,53 @@
 import json
+import logging
 import os
 import shutil
+import subprocess
 import tempfile
 
 import requests
 
-from git import Repo
 
 from .docker import is_docker
 
-base = 'https://renkulab.io/knowledge-graph/workflows/'
+from git import Repo
+from renku.command.graph import export_graph_command
+
+GRAPH_HOST_PATH = "/tmp/omni-graphs"
+GRAPH_CONT_PATH = "/graph"
+GRAPH_JSON = "graph.json"
 
 def run():
     if is_docker():
-        return run_in_docker(export=True)
+        return run_from_docker(export=True)
     else:
-        print("> not in docker")
+        print("> running in host")
+        shutil.rmtree(GRAPH_HOST_PATH, ignore_errors=True)
+        os.makedirs(GRAPH_HOST_PATH, exist_ok=True)
+        # TODO get image from cli
+        docker_image = "renku-csf-ext"
+        out = run_from_host_in_docker(docker_image)
+        summarize_graph()
+        return out
 
-def run_in_docker(full=True, export=False):
-    print("> run in docker")
+def run_from_docker(full=True, export=False):
+    print("> running in docker")
+    # TODO do actual run    
     if export:
         return export_graph(full=True)
 
-def export_graph(full=True):
-    from renku.command.graph import export_graph_command
+def run_from_host_in_docker(docker_image):
+    p = subprocess.run([
+            "docker", "run",
+            "--rm", "-v", "{graph_host}:{graph_container}".format(
+                graph_host=GRAPH_HOST_PATH,
+                graph_container=GRAPH_CONT_PATH),
+            "-v", ".:/home/rstudio/work",
+            "-e", "OMNICLI_GRAPH_PATH={graph_container}".format(graph_container=GRAPH_CONT_PATH),
+            docker_image])
+    return p.stdout
 
+def export_graph(full=True):
     if full:
         revision = None
 
@@ -41,7 +64,7 @@ def export_graph(full=True):
     output_dir = get_graph_output_dir()
 
     if output_dir:
-        with open(os.path.join(output_dir, 'graph.jsonl'), 'w') as f:
+        with open(os.path.join(output_dir, GRAPH_JSON), 'w') as f:
             f.write(result)
         return "ok"
     else:
@@ -49,3 +72,10 @@ def export_graph(full=True):
 
 def get_graph_output_dir():
     return os.environ.get('OMNICLI_GRAPH_PATH', None)
+
+def summarize_graph():
+    from rdflib import Graph
+    g = Graph()
+    g.parse("file://{path}/graph.jsonl".format(path=GRAPH_HOST_PATH), format="json-ld")
+    graph_len = len(list(g.triples((None, None, None))))
+    print("> Got", graph_len, "triples")
